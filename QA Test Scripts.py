bb2c5169575a75330419a16b8cb63d955be0c93a
cheskalazo <57380689+cheskalazo@users.noreply.github.com>
2020-12-26 12:19:27 +0800

    Added QA Test Script

---
+++ QA Test Scripts.py
@@ -0,0 +1,19 @@
+import pytest 
+# from RatVenture_Function import * # update once developer starts 
+
+'''''
+Sprint 1
+'''''
+
+def test_main_menu(): 
+    value = open(main_menu)
+    assert value == "Welcome to Ratventure! /n" 
+                     "----------------------"
+                     "1) New Game /n"
+                     "2) Resume Game /n"
+                     "3) Exit Game /n"
+                     "Enter choice: "
+                
+                     
+    
+def tes_new_game(): 
