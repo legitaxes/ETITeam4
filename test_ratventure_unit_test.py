b5d4f76225f4e825669f46bcecc51ed6dd4e4f20
Jia Qi Poh <s10189672@connect.np.edu.sg>
2020-12-28 00:41:57 +0800

    created issue and comments for the func to be done

--- test_ratventure_unit_test.py
+++ test_ratventure_unit_test.py
@@ -40,6 +40,10 @@ def test_rat():
 
 
 def test_world_map():
+    """
+    Test function of initializing world_map():
+        The map will be initialized upon running this function
+    """
     value = world_map()
     expected = [['T', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
                 [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],\
@@ -51,3 +55,57 @@ def test_world_map():
                 [' ', ' ', ' ', ' ', ' ', ' ', ' ', 'K']]
     assert len(value) == len(expected)
     assert all([a == b for a, b in zip(value, expected)]) #this checks python list against the expected value
+
+
+def test_print_map():
+    """
+    Test function of print_map Function:
+        Displays the Map of the game when called
+        This function should print the full layout of the map
+    """
+    # TODO Create a test case that prints the map of the game 
+    # Test case should assert the following:
+    #  > The map 
+    #  > Formatting of the "borders" 
+    #  > Displaying the "legends"
+    # labels: tasks, unit-test
+    # assignees: legitaxes
+
+
+def test_print_day():
+    """
+    Test function of print_day Function:
+        Display the tile the hero is at and display whether the hero is in town or out in open
+    """
+    # TODO Create a test case that prints the day of the game
+    # Test case should  assert the following values:
+    #  > Location of the hero 
+    #  > Displaying the day
+    # labels: tasks, unit-test
+    # assignees: laukwangwei
+
+
+def test_print_hero_stats():
+    """
+    Test function of print_hero_stats Function:
+        Display the hero's stats and his details
+        This function should return the hero's Name, Damage, Defence and HP 
+    """
+    # TODO Create a test case that prints the stats of the hero
+    # Test case should assert the following values:
+    #  > All of the hero's stats
+    # labels: tasks, unit-test
+    # assignees: laukwangwei
+
+def test_get_hero_position():
+    """
+    Test function of get_hero_position Function:
+        This function mainly serves as a way for the program to get the position of the hero
+        It should return the tile where the hero is on the map
+    """
+    # TODO Create a test case that gets the hero's position
+    # Test case should assert the following values:
+    #   > The position of the hero
+    # labels: tasks, unit-test
+    # assignees: legitaxes
+    
